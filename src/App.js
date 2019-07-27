c940913d0666237b55a66d05faaeb79ee0ac2937
Abhishek <abhishekthakurbir@gmail.com>
2019-07-28 00:20:11 +0530

    Added changes to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -52,18 +52,22 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render = { () => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />) } />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser //get currentUser from store i.e. destructure user object from root object
+});
+
 const mapDispatchToProps = dispatch => ({ //Dispatch is action object which is passed to every reducer by redux
   setCurrentUser: user => dispatch(setCurrentUser(user)) //setCurrentUser to object in setCurrentUser() from user actions
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
