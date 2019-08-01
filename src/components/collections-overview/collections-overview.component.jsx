31dd33befb70abbd22af7525b4b91d4fba72f9ea
abhishekthakur7 <abhishekthakurbir@gmail.com>
2019-08-02 01:32:45 +0530

    added collection component, updated selectors and corresponding reducers

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selector';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selector';
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
-    <div className='shop-page'>
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
-
-    </div>
+  <div className='collections-overview'>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -6,3 +6,12 @@ export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections 
 );
+
+export const selectCollectionsForPreview = createSelector(  //convert collections object to array
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => createSelector( //select a single collectoion (hats/sneakers etc. from collections object)
+  [selectCollections],
+  collections => collections[collectionUrlParam]);
