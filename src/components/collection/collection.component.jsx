381e3ae1057ee9e6752259003c8c1306c9f408aa
Abhishek Thakur <abhishekthakurbir@gmail.com>
2019-08-11 16:02:40 +0530

    1. Moved shopdata to firestore 2. Implemented HOC to show spinner when loading shopdata from firestore

--- src/components/collection/collection.component.jsx
+++ src/components/collection/collection.component.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import { CollectionPageContainer, TitleContainer, ItemsContainer } from './collection.styles';
-
-import CollectionItem from '../collection-item/collection-item.component';
-
 import { connect } from 'react-redux';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
 import { selectCollection } from '../../redux/shop/shop.selector';
 
-const CollectionPage = ({ match, collection }) => {
+import {
+    CollectionPageContainer,
+    TitleContainer,
+    ItemsContainer
+} from './collection.styles';
+
+const CollectionPage = ({ collection }) => {
     const { title, items } = collection;
     return (
         <CollectionPageContainer>
-            <TitleContainer> {title} </TitleContainer>
+            <TitleContainer>{title}</TitleContainer>
             <ItemsContainer>
-                {items.map(item => <CollectionItem key={item.id} item={item} />)}
+                {items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
             </ItemsContainer>
         </CollectionPageContainer>
-    )
+    );
 };
 
-
-const mapStateToProps = (state, ownProps) => ({   //mapStateToProps can accept two params, ownProps is current component's props
-    collection: selectCollection(ownProps.match.params.collectionId)(state) //here two params are passed because this selector needs a part of the state depending upon the url param
-});
+const mapStateToProps = (state, ownProps) => {
+    return ({  //mapStateToProps can accept two params, ownProps is current component's props
+        collection: selectCollection(ownProps.match.params.collectionId)(state) //here two params are passed because this selector needs a part of the state depending upon the url param
+    })
+};
 
 export default connect(mapStateToProps)(CollectionPage);
--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -4,15 +4,56 @@ import { Route } from 'react-router-dom';
 
 import CollectionOverview from '../collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
-const ShopPage = ({ match }) => {
-    console.log(match);
-    return ( //Route component in App.js will pass three props i.e. match, history & location
-        <div className='shop-page'>
-            <Route exact path={`${match.path}`} component={CollectionOverview} />
-            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-        </div>
-    )
-};
+import WithSpinner from '../with-spinner/with-spinner.component';
 
-export default ShopPage;
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
+
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+
+    state = {
+        loading: true
+    };
+
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount() {
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection('collections');
+        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+            this.setState({ loading: false });
+        });
+    }
+
+    render() {
+        const { match } = this.props;
+        const { loading } = this.state;
+        return ( //Route component in App.js will pass three props i.e. match, history & location
+            <div className='shop-page'>
+                <Route 
+                    exact 
+                    path={`${match.path}`} 
+                    render={props => 
+                                <CollectionOverviewWithSpinner isLoading={loading} {...props} />} />
+                <Route 
+                    path={`${match.path}/:collectionId`} 
+                    render={props => 
+                                <CollectionPageWithSpinner isLoading={loading} {...props} />}  />
+            </div>
+        )
+    }
+
+}
+
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
